79fec3e21a54dc76613f7c1329bc98ada64cb6f7
theMightyT <tvanrys@fanshaweonline.ca>
2019-11-20 14:05:00 -0500

    remove socket

--- index.js
+++ index.js
@@ -29,26 +29,26 @@ app.get('/api', (req, res) => {
 	})
 })
 
-// plug in socket.io
-io.on('connection', function(socket) {
-	console.log('a user has connected');
-	io.emit('chat message', { for: 'everyone', message: `${socket.id} is here to par TAY!!` });
+// // plug in socket.io
+// io.on('connection', function(socket) {
+// 	console.log('a user has connected');
+// 	io.emit('chat message', { for: 'everyone', message: `${socket.id} is here to par TAY!!` });
 
-	// listen for a message, and then send it where it needs to go
-	socket.on('chat message', function(msg) {
-		console.log('message: ', msg);
+// 	// listen for a message, and then send it where it needs to go
+// 	socket.on('chat message', function(msg) {
+// 		console.log('message: ', msg);
 
-		// send a message event to all clients
-		io.emit('chat message', { for: 'everyone', message: msg });
-	});
+// 		// send a message event to all clients
+// 		io.emit('chat message', { for: 'everyone', message: msg });
+// 	});
 
-	// listen for disconnet
-	socket.on('disconnect', function() {
-		console.log('a user disconnected');
-		msg = `${socket.id} has left the building!`;
-		io.emit('disconnect message', msg);
-	});
-});
+// 	// listen for disconnet
+// 	socket.on('disconnect', function() {
+// 		console.log('a user disconnected');
+// 		msg = `${socket.id} has left the building!`;
+// 		io.emit('disconnect message', msg);
+// 	});
+// });
 
 // tell the app to be served up at this port (same as WAMP or MAMP, just a different port)
 http.listen(port, function() {
